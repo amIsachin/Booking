75b2a0dc08b551a3bf2476f48045806c9c916946
amIsachin <amIsachin@hotmail.com>
2022-08-25 15:02:50 +0530

    some changes almost auditable base entity final delete base entity class

--- Domain/Common/AuditableBaseEntity.cs
+++ Domain/Common/AuditableBaseEntity.cs
@@ -2,15 +2,11 @@
 
 namespace Domain.Common
 {
-    public abstract class AuditableBaseEntity : BaseEntity
+    public abstract class AuditableBaseEntity
     {
         public DateTime Created { get; set; }
         public string CreatedBy { get; set; }
         public DateTime LastModified { get; set; }
         public string LastModifiedBy { get; set; }
-
-
-        public int ID { get; set; }
-        public DateTime CreatedOn { get; set; } = DateTime.Now;
     }
 }
