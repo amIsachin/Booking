543b8dd5585f54039475a6953a80f217beddfaa7
amIsachin <amIsachin@hotmail.com>
2022-09-27 14:17:12 +0530

    Add Combination base entity for some common property present in driver and rider entity but right now not use combination base entity All Changes in Domain layer

---
+++ Domain/Common/CommonEntities/CombinationBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Common.CommonEntities
+{
+    public abstract class CombinationBaseEntity : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+    }
+}
