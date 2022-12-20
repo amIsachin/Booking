412a2271ffbe500acf74a395c983246e63c6707d
amIsachin <amIsachin@hotmail.com>
2022-12-20 14:03:06 +0530

    create folder in domain layer global entities just little changes in driver controller but done

--- Booking.Web/Controllers/DriverController.cs
+++ Booking.Web/Controllers/DriverController.cs
@@ -4,7 +4,20 @@ namespace Booking.Web.Controllers
 {
     public class DriverController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create(int a)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
         {
             return View();
         }
