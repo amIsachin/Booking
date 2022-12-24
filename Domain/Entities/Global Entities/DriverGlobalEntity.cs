d5b204c3c7e660f77499a5436f1cafe2e7acd9ec
amIsachin <amIsachin@hotmail.com>
2022-12-24 14:08:17 +0530

    some small changes and just read the code.

--- Domain/Entities/Global Entities/DriverGlobalEntity.cs
+++ Domain/Entities/Global Entities/DriverGlobalEntity.cs
@@ -5,6 +5,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities.Global_Entities
 {
+    /// <summary>
+    /// This entity is global entity you can use for global application.
+    /// this Enitty use everywhere. not restriction for this entity.
+    /// </summary>
     public class DriverGlobalEntity : CloneableBaseEntity
     {
         [Key]
