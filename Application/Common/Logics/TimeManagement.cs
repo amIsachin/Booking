475348bc592e2da874dad5a385bf5ec4699ae904
amIsachin <amIsachin@hotmail.com>
2022-12-28 14:21:10 +0530

    Time management class work done, i will be improve single ton after some time

---
+++ Application/Common/Logics/TimeManagement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Application.Common.Logics
+{
+    /// <summary>
+    /// This *(Time Management) class contains only related time scheduling for entire application.
+    /// This class is sealed so please do not inherited any other class.
+    /// This class is *(Single Ton) class.
+    /// </summary>
+    public sealed class TimeManagement
+    {
+        #region SingleTon
+        private static TimeManagement instance = null;
+        public static TimeManagement Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TimeManagement();
+                }
+
+                return instance;
+            }
+        }
+
+        private TimeManagement()
+        {
+        } 
+        #endregion
+
+        /// <summary>
+        /// this method is return date time now *(Microsoft system property).
+        /// Use this method when the new driver and rider enroll first time.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime DateTimeNow()
+        {
+            return DateTime.Now;
+        }
+
+        /// <summary>
+        /// this method is return date time now *(Microsoft system property).
+        /// Use this method when the driver and rider modified existing value respectively.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime LastModified()
+        {
+            return DateTime.Now;
+        }
+    }
+}
