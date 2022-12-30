431670ccb2e122c435ad08692ef6e37adfc371e3
amIsachin <amIsachin@hotmail.com>
2022-12-30 15:08:27 +0530

    Create one method insert driver in application layer and some small changes..

--- Application/Common/Interfaces/Driver/ICreateDriverCommand.cs
+++ Application/Common/Interfaces/Driver/ICreateDriverCommand.cs
@@ -1,9 +1,12 @@
-﻿namespace Application.Common.Interfaces.Driver
+﻿using Domain.Entities.SystemEntities;
+
+namespace Application.Common.Interfaces.Driver
 {
     /// <summary>
     /// This *(ICreateDriverCommand) interface is main *(service) which saves the driver data in the database directly.
     /// </summary>
     public interface ICreateDriverCommand
     {
+        int InsertDriver(DriverSystemEntity driverSystemEntity);
     }
 }
--- Application/Drivers/Commands/CreateDriver/CreateDriverCommand.cs
+++ Application/Drivers/Commands/CreateDriver/CreateDriverCommand.cs
@@ -1,11 +1,23 @@
 ﻿using Application.Common.Interfaces.Driver;
+using Domain.Entities.SystemEntities;
+using Infrastructure.Persistence;
 
 namespace Application.Drivers.Commands.CreateDriver
 {
     /// <summary>
     /// This *(CreateDriverCommand) class is main *(service) which saves the driver data in the database directly.
     /// </summary>
-    public class CreateDriverCommand : ICreateDriverCommand
+    public sealed class CreateDriverCommand : ICreateDriverCommand
     {
+        private readonly ApplicationDbContext _driverService;
+        public CreateDriverCommand(ApplicationDbContext driverService)
+        {
+            this._driverService = driverService;
+        }
+
+        public int InsertDriver(DriverSystemEntity driverSystemEntity)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
